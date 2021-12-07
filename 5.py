1c2555596d03c2317b0473ad17c12da8b3b63d2b
Vaslera <90094359+Vaslera@users.noreply.github.com>
2021-12-07 09:06:05 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,22 +1,35 @@
-d = [1, 9, 2, 8, 3, 7, 4, 6, 5]
-d = sorted(d)
+d = [-42, -42, -42, -42, -42]
 def binary_search(d, n):
-    lower = 0
+    lower = -1
     upper = len(d) - 1
-    while lower <= upper:
+    while lower < upper - 1:
         center = (lower + upper) // 2
-        if d[center] == n:
-            return center
-        elif d[center] > n:
-            upper = center - 1
-        elif d[center] < n:
-            lower = center + 1
-    return None
+        if d[center] >= n:
+            upper = center
+        else:
+            lower = center
+    if upper >= 0 and d[upper] == n:
+        return upper
+    else:
+        return None
 
 
-n = int(input("Элемент, который необходимо обнаружить: "))
+n = int(input("Элемент, который необходимо найти: "))
 print(binary_search(d, n))
-assert binary_search([1, 9, 2, 8, 3, 7, 4, 6, 5], 1) == 0
-assert binary_search([1, 9, 2, 8, 3, 7, 4, 6, 5], 0) == None
-assert binary_search([], 1) == None
-assert binary_search([1, 9, 2, 8, 3, 7, 1, 4, 6, 5], 1) == 0
+assert binary_search([], 42) == None
+assert binary_search([0], 0) == 0
+assert binary_search([0], 1) == None
+assert binary_search([-1, 0, 42], 0) == 1
+assert binary_search([-42, 0, 42], 42) == 2
+assert binary_search([-6, -5, -4, -3, -2, -1], -4) == 2
+assert binary_search([1, 2, 3, 4, 5, 6], 4) == 3
+assert binary_search([1, 2, 3, 4, 5, 6, 7], 4) == 3
+assert binary_search([1, 2, 3, 4, 5], 7) == None
+assert binary_search([1, 2, 3, 4, 5, 6], 7) == None
+assert binary_search([42, 42, 42, 42, 42], 42) == 0
+assert binary_search([-42, -42, -42, -42, -42], -42) == 0
+assert binary_search([42, 42, 42, 42, 43], 43) == 4
+assert binary_search([41, 42, 42, 42, 42], 41) == 0
+assert binary_search([-2, -2, -1, 0, 1, 2, 2, 2], -1) == 2
+assert binary_search([-2, -2, -1, 0, 1, 1, 2, 2], 1) == 4
+assert binary_search([2, 2, 2, 2, 2, 2], 2) == 0
